ce6388851989071b27ce3a2c4c5afb6edf0c9d93
chudjakd <chudjakd@gmail.com>
2020-08-31 14:46:52 +0200

    Vymazal som zbytocnu metodu

--- src/main/java/metody/metodyPreSrandu.java
+++ src/main/java/metody/metodyPreSrandu.java
@@ -2,9 +2,6 @@ package metody;
 
 public class metodyPreSrandu {
 
-    public static void zbytocnaMetoda(){
-        System.out.println("Nerobim vobec nic");
-    }
     public static void metodaSkuska(){
         System.out.println("Metoda zo skusky");
     }
