7151d275732680e18a02027cc54d152bbdc3e7b7
chudjakd <chudjakd@gmail.com>
2020-08-31 14:38:29 +0200

    Nova metoda v metodach pre skusky s nazvom metodaskuska vytvorena

--- src/main/java/metody/metodyPreSrandu.java
+++ src/main/java/metody/metodyPreSrandu.java
@@ -5,6 +5,9 @@ public class metodyPreSrandu {
     public static void zbytocnaMetoda(){
         System.out.println("Nerobim vobec nic");
     }
+    public static void metodaSkuska(){
+        System.out.println("Metoda zo skusky");
+    }
 
 }
 
