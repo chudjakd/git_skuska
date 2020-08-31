30780e5bc8e01d8bbae59a73d679eafa2b445e28
chudjakd <chudjakd@gmail.com>
2020-08-31 14:41:20 +0200

    Nova metoda v metodach pre skusky s nazvom metodavasko vytvorena

--- src/main/java/metody/metodyPreSrandu.java
+++ src/main/java/metody/metodyPreSrandu.java
@@ -4,4 +4,8 @@ public class metodyPreSrandu {
     public static void thisMethodIsUseful(){
         System.out.println("2 +2 je 4");
     }
+
+    public static void metodaVasko(){
+        System.out.println("Metoda z vasko");
+    }
 }
