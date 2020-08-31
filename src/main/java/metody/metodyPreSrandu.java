4d1bbc27989204c18942d348f5814658b20d052b
chudjakd <chudjakd@gmail.com>
2020-08-31 14:51:58 +0200

    Dalsia metoda vo vaskovi

--- src/main/java/metody/metodyPreSrandu.java
+++ src/main/java/metody/metodyPreSrandu.java
@@ -6,4 +6,8 @@ public class metodyPreSrandu {
     public static void metodaVasko(){
         System.out.println("Metoda z vasko");
     }
+
+    public static void dalsiaMetodaVasko(){
+        System.out.println("Hmm toto je dalsia metoda vo vasko");
+    }
 }
